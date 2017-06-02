96671f87fff75130aa67abbada08a264dc9ac294
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-02 11:33:18 -0500

    first commit

---
+++ src/main/java/apprenticeship/Grid.java
@@ -0,0 +1,7 @@
+package apprenticeship;
+
+/**
+ * Created by sabbas on 6/2/17.
+ */
+public class Grid {
+}
---
+++ src/test/java/apprenticeship/GridTest.java
@@ -0,0 +1,13 @@
+package apprenticeship;
+
+import org.junit.Test;
+
+
+public class GridTest {
+    @Test
+    public void canDrawVerticalLine() {
+        Grid threeByThree = new Grid();
+        threeByThree.drawVerticalLines();
+
+    }
+}
