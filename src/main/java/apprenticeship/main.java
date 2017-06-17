f45d7439913b23feaed5c437ee4ea23469d61058
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-17 11:54:55 -0500

    add the validator class to the branch

--- src/main/java/apprenticeship/main.java
+++ src/main/java/apprenticeship/main.java
@@ -2,9 +2,6 @@ package apprenticeship;
 
 public class main {
     public static void main (String arg[]) {
-//        ScannerImplementation scanner = new ScannerImplementation();
-//        UserInputReceiver input = new UserInputReceiver();
-//        int cell = input.obtainCellSelection();
-//        System.out.println("You selected: " + cell);
+
     }
 }
---
+++ src/test/java/apprenticeship/UserInputValidatorTest.java
@@ -0,0 +1,81 @@
+package apprenticeship;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class UserInputValidatorTest {
+
+    private UserInputValidator userInputValidator;
+
+    @Before
+    public void canCreateValidator() {
+        userInputValidator = new UserInputValidator();
+    }
+
+    @Test
+    public void returnTrueWhenInputSymbolIsX() {
+        String user_input = "X";
+        boolean result = userInputValidator.validateSymbolSelection(user_input);
+        assertEquals(result, true);
+    }
+
+    @Test
+    public void returnTrueWhenInputSymbolIsO() {
+        String user_input = "O";
+        boolean result = userInputValidator.validateSymbolSelection(user_input);
+        assertEquals(result, true);
+    }
+
+    @Test
+    public void returnFalseWhenInputSymbolIsAnIncorrectString() {
+        String user_input = "XOXOXO";
+        boolean result = userInputValidator.validateSymbolSelection(user_input);
+        assertEquals(result, false);
+    }
+
+    @Test
+    public void returnFalseWhenInputSymbolIsAnEmptyString() {
+        String user_input = "";
+        boolean result = userInputValidator.validateSymbolSelection(user_input);
+        assertEquals(result, false);
+    }
+
+    @Test
+    public void returnFalseWhenInputSymbolIsASpecialCharacter() {
+        String user_input = "#";
+        boolean result = userInputValidator.validateSymbolSelection(user_input);
+        assertEquals(result, false);
+    }
+
+    @Test
+    public void returnFalseWhenInputSymbolIsANewLineCharacter() {
+        String user_input = "\n";
+        boolean result = userInputValidator.validateSymbolSelection(user_input);
+        assertEquals(result, false);
+    }
+
+    @Test
+    public void returnTrueWhenInputCellSelectionIsInRange() {
+        int user_input = 1;
+        boolean result = userInputValidator.validateCellSelection(user_input);
+        assertEquals(result, true);
+    }
+
+    @Test
+    public void returnFalseWhenInputCellSelectionIsOutOfRange() {
+        int user_input = 10;
+        boolean result = userInputValidator.validateCellSelection(user_input);
+        assertEquals(result, false);
+    }
+
+    @Test
+    public void returnFalseWhenInputCellSelectionIsANegativeNumber() {
+        int user_input = -10;
+        boolean result = userInputValidator.validateCellSelection(user_input);
+        assertEquals(result, false);
+    }
+}
