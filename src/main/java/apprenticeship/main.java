0effbccfd4f799bda864809a7e2e154790ee52a0
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-18 22:59:50 -0500

    test run

--- src/main/java/apprenticeship/main.java
+++ src/main/java/apprenticeship/main.java
@@ -1,7 +1,15 @@
 package apprenticeship;
 
+import java.util.Scanner;
+
 public class main {
     public static void main (String arg[]) {
+        RulesFor3x3 threeThreeRules = new RulesFor3x3();
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        UserInput receiver = new UserInput(scanner);
 
+        Game tictactoe = new Game(board, threeThreeRules, receiver);
+        tictactoe.play();
     }
 }
