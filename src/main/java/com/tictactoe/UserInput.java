da18c904aa6debae82bc06a33ee93a6a74c1031f
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-21 23:02:08 -0500

    catch inputMismatchException

--- src/main/java/com/tictactoe/UserInput.java
+++ src/main/java/com/tictactoe/UserInput.java
@@ -1,5 +1,6 @@
 package com.tictactoe;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInput {
@@ -11,8 +12,17 @@ public class UserInput {
     }
 
     public int obtainValidCellSelection() {
+        int cell;
         System.out.println("Enter your cell selection (1 - 9): ");
-        int cell = this.scanner.nextInt();
+        while (true)  {
+            try {
+                cell = this.scanner.nextInt();
+                break;
+            } catch (InputMismatchException ime) {
+                System.out.println("Expected int, got String. Enter a number between 1 and 9: ");
+                this.scanner.next();
+            }
+        }
         boolean isValid = validator.validateCellSelection(cell);
         if (!isValid) {
             System.out.println ("Invalid cell number. Choose one of the available cells between 1 and 9");
