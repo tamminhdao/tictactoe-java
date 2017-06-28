9502ec946f67bba5f20fec53c59eafd187d8e71f
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-28 09:43:03 -0500

    create class Program and initialize everything in Program

--- src/main/java/com/tictactoe/main.java
+++ src/main/java/com/tictactoe/main.java
@@ -1,20 +1,8 @@
 package com.tictactoe;
 
-import java.util.Scanner;
-
 public class main {
     public static void main (String arg[]) {
-        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
-        UserInput receiver = new UserInput(scanner);
-        Board board = new Board();
-        UserInputValidator validator = new UserInputValidator(board);
-        GameSettings menu = new GameSettings(receiver, validator);
-        GamePreference preference = menu.collectGamePreference();
-
-        HumanPlayer player1 = new HumanPlayer(receiver, preference.player1Symbol, validator);
-        HumanPlayer player2 = new HumanPlayer(receiver, preference.player2Symbol, validator);
-
-        Game tictactoe = new Game(player1, player2, board);
-        tictactoe.play();
+        Program program = new Program();
+        program.run();
     }
 }
