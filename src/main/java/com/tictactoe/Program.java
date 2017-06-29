aeac7ba72a6b54e9a3125f44725b25d501c54fa8
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-29 15:07:02 -0500

    test run

--- src/main/java/com/tictactoe/Program.java
+++ src/main/java/com/tictactoe/Program.java
@@ -10,8 +10,12 @@ public class Program {
     private GameSettings menu = new GameSettings(receiver, validator);
     private GamePreference preference = menu.collectGamePreference();
 
-    private HumanPlayer player1 = new HumanPlayer(receiver, preference.player1Symbol, validator);
-    private HumanPlayer player2 = new HumanPlayer(receiver, preference.player2Symbol, validator);
+//    private HumanPlayer player1 = new HumanPlayer(receiver, preference.player1Symbol, validator);
+//    private HumanPlayer player2 = new HumanPlayer(receiver, preference.player2Symbol, validator);
+
+
+    private Player player1 = new EasyComputerPlayer("X", board);
+    private Player player2 = new EasyComputerPlayer("O", board);
 
     private Game tictactoe = new Game(player1, player2, board);
 
