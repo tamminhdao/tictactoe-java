032b899bc2086c2b8e072ecc829b3f19da755a0d
Tam Dao <tamminhdaoabbas@gmail.com>
2017-06-29 16:12:05 -0500

    edit message printed to the console

--- src/main/java/com/tictactoe/GameSettings.java
+++ src/main/java/com/tictactoe/GameSettings.java
@@ -33,7 +33,7 @@ public class GameSettings {
     }
 
     private String chooseSymbol(String playerId) {
-        System.out.println(playerId + " - Pick your unique symbol: ");
+        System.out.println("Pick an unique symbol for " + playerId);
         String playerSymbol = this.getInput();
         boolean validSymbol = validator.validateSymbolSelection(playerSymbol);
         while (!validSymbol) {
