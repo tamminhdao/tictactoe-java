b3871c6bbcdc831436d63c076885c93c264fb4e7
Tam Dao <tamminhdaoabbas@gmail.com>
2017-07-13 14:22:02 -0500

    validate game type input

--- src/main/java/com/tictactoe/OptionsMenu.java
+++ src/main/java/com/tictactoe/OptionsMenu.java
@@ -9,7 +9,7 @@ public class OptionsMenu {
     private UserInputValidator validator = new UserInputValidator(board);
     private GameSettings menu = new GameSettings(receiver, validator);
 
-    public String getInput() {
+    private String getInput() {
         return this.receiver.obtainInput();
     }
 
@@ -19,10 +19,31 @@ public class OptionsMenu {
         System.out.println("2. Human Versus Computer");
         System.out.println("3. Computer Versus Human");
         System.out.println("4. Computer Versus Computer");
+    }
+
+    private int obtainValidGameTypeSelection() {
+        int gameType = 0;
         System.out.println("Select one of the game types by picking a corresponding number [1-4]");
+        boolean waitingForInput = true;
+        while (waitingForInput) {
+            try {
+                String input = getInput();
+                gameType = Integer.parseInt(input);
+                waitingForInput = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Input must be a number");
+            }
+        }
+        boolean isValid = validator.validateGameTypeSelection(gameType);
+        if (!isValid) {
+            System.out.println("The number you pick does not associate with any game type.");
+            gameType = obtainValidGameTypeSelection();
+        }
+        return gameType;
     }
 
     private void humanVsHuman() {
+        System.out.println("Starting a human vs. human game!");
         GamePreference preference = menu.collectGamePreference();
         Player player1 = new HumanPlayer (receiver, preference.player1Symbol, validator);
         Player player2 = new HumanPlayer (receiver, preference.player2Symbol, validator);
@@ -31,6 +52,7 @@ public class OptionsMenu {
     }
 
     private void humanVsComputer() {
+        System.out.println("Starting a human vs. computer game!");
         GamePreference preference = menu.collectGamePreference();
         Player player1 = new HumanPlayer (receiver, preference.player1Symbol, validator);
         Player player2 = new EasyComputerPlayer (preference.player2Symbol, board);
@@ -39,6 +61,7 @@ public class OptionsMenu {
     }
 
     private void computerVsHuman() {
+        System.out.println("Starting a computer vs. human game!");
         GamePreference preference = menu.collectGamePreference();
         Player player1 = new EasyComputerPlayer (preference.player1Symbol, board);
         Player player2 = new HumanPlayer (receiver, preference.player2Symbol, validator);
@@ -47,6 +70,7 @@ public class OptionsMenu {
     }
 
     private void computerVsComputer() {
+        System.out.println("Starting a computer vs. computer game!");
         GamePreference preference = menu.collectGamePreference();
         Player player1 = new EasyComputerPlayer (preference.player1Symbol, board);
         Player player2 = new EasyComputerPlayer (preference.player2Symbol, board);
@@ -56,8 +80,7 @@ public class OptionsMenu {
 
     public void open() {
         listAllOptions();
-        String input = getInput();
-        int gameType = Integer.parseInt(input);
+        int gameType = obtainValidGameTypeSelection();
         switch (gameType) {
             case 1: humanVsHuman();
             break;
