33dd1c66a20b13a02e56a470c866638f53fb4ed8
Tam Dao <tamminhdaoabbas@gmail.com>
2017-07-23 20:37:31 -0500

    start implementing medium computer player

--- src/main/java/com/tictactoe/MediumComputerPlayer.java
+++ src/main/java/com/tictactoe/MediumComputerPlayer.java
@@ -1,7 +1,76 @@
 package com.tictactoe;
+import java.util.*;
 
-/**
- * Created by tamdao on 7/23/17.
- */
-public class MediumComputerPlayer {
+public class MediumComputerPlayer implements Player, SmartAI {
+    private Board board;
+    private String selfSymbol;
+    private String opponentSymbol;
+    private Rules rules;
+
+    public MediumComputerPlayer(Board board) {
+        this.board = board;
+    }
+
+    @Override
+    public void addRules(Rules rules) {
+        this.rules = rules;
+    }
+
+    @Override
+    public void addSymbol(String symbol) {
+        this.selfSymbol = symbol;
+    }
+
+    @Override
+    public void addOpponentSymbol(String opponentSymbol) {
+        this.opponentSymbol = opponentSymbol;
+    }
+
+    @Override
+    public String getSymbol() {
+        return this.selfSymbol;
+    }
+
+    @Override
+    public int obtainValidCellSelection() {
+        int CELL_OFFSET = 1;
+        HashMap<Integer, Integer> scoreAndCell = new HashMap<>();
+        for (int index = 0; index < board.getBoardSize(); index++) {
+            if (isEmptyCell(board, index)) {
+                board.insertSymbol(selfSymbol, index);
+                int score = scoreEachCell(board);
+                scoreAndCell.put(score, index);
+                board.resetCell(index);
+            }
+        }
+        Set allKeys = scoreAndCell.keySet();
+        ArrayList<Integer> listOfKeys = new ArrayList<>(allKeys);
+        int maxScore = Collections.max(listOfKeys);
+        int cell = scoreAndCell.get(maxScore);
+        return cell + CELL_OFFSET;
+    }
+
+    private boolean thereIsAWinner() {
+        String winner = this.rules.checkForWinner(this.board);
+        return !winner.equals("");
+    }
+
+    private boolean isEmptyCell(Board board, int index) {
+        return board.getSymbol(index).equals(" ");
+    }
+
+    private int getWinOrLoseScore() {
+        String winner = this.rules.checkForWinner(this.board);
+        if (winner.equals(selfSymbol)) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public int scoreEachCell(Board board) {
+        if (thereIsAWinner()) {
+            return getWinOrLoseScore();
+        }
+        return 0;
+    }
 }
--- src/test/java/com/tictactoe/MediumComputerPlayerTest.java
+++ src/test/java/com/tictactoe/MediumComputerPlayerTest.java
@@ -1,7 +1,24 @@
 package com.tictactoe;
 
-/**
- * Created by tamdao on 7/23/17.
- */
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class MediumComputerPlayerTest {
+    private final int CELL_OFFSET = 1;
+
+    @Test
+    public void mediumLevelPlayerCanBlockAWin() throws Exception {
+        Board board = new Board();
+        Rules rules = new RulesFor3x3();
+        MediumComputerPlayer mediumAI = new MediumComputerPlayer(board);
+        mediumAI.addRules(rules);
+        mediumAI.addSymbol("AI");
+        mediumAI.addOpponentSymbol("O");
+        board.insertSymbol("O", 1);
+        board.insertSymbol("O", 4);
+        int cellSelection = mediumAI.obtainValidCellSelection();
+        int cellIndex = cellSelection - CELL_OFFSET;
+        assertEquals(7, cellIndex);
+    }
 }
