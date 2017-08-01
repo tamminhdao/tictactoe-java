abb9cbc345b78e91643ab9345edb5f82f7bd81db
Tam Dao <tamminhdaoabbas@gmail.com>
2017-08-01 07:02:36 -0500

    test converting json to object

---
+++ src/main/java/com/tictactoe/Messages.java
@@ -0,0 +1,17 @@
+package com.tictactoe;
+
+public class Messages {
+    public String GameSettings_checkForUniqueSymbols;
+    public String GameSettings_chooseSymbol;
+    public String GameSettings_invalidSymbol;
+    public String GameSettings_pickPlayerType;
+    public String GameSettings_askToPlayAgain;
+    public String GameSettings_goodbye;
+    public String Game_gameIntro;
+    public String Game_announceWinner;
+    public String Game_announceTie;
+    public String Game_notifyPlayersTurn;
+    public String HumanPlayer_obtainValidCellSelection;
+    public String HumanPlayer_numberFormatException;
+    public String HumanPlayer_invalidCellNumber;
+}
