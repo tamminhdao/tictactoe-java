b7cbaffa805712f53226654edc05f3d410f2eb1b
Tam Dao <tamminhdaoabbas@gmail.com>
2017-08-02 15:07:23 -0500

    able to add files and read from language folders

--- src/main/java/com/tictactoe/LanguageMenu.java
+++ src/main/java/com/tictactoe/LanguageMenu.java
@@ -1,40 +1,53 @@
 package com.tictactoe;
 
 import com.google.gson.Gson;
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class LanguageMenu {
     private UserInput receiver;
     private Gson gson = new Gson();
+    private ArrayList<String> availableLanguages = new ArrayList<>();
 
     public LanguageMenu(UserInput receiver) {
         this.receiver = receiver;
+        listFilesFromFolder(new File("languageFiles"));
     }
 
     public Messages chooseLanguage() {
-        System.out.println("Press [E] for English or [V] for Vietnamese");
+        System.out.println("Pick a language from below and enter it in the console.");
+        displayAvailableLanguageOptions();
         String languageSelection = this.getInput();
-        if (languageSelection.equals("E")) {
-            try {
-                Messages messages = gson.fromJson(new FileReader("languageFiles/english.json"), Messages.class);
-                return messages;
-            } catch (FileNotFoundException fnf) {
-                System.out.println("Language not available.");
-            }
-        } else if (languageSelection.equals("V")) {
-            try {
-                Messages messages = gson.fromJson(new FileReader("languageFiles/vietnamese.json"), Messages.class);
-                return messages;
-            } catch (FileNotFoundException fnf) {
-                System.out.println("Language not available.");
-            }
+        try {
+            String path = "languageFiles/" + languageSelection + ".json";
+            return gson.fromJson(new FileReader(path), Messages.class);
+        } catch (FileNotFoundException fnf) {
+            System.out.println("The application is not yet available in this language. \n");
         }
         return chooseLanguage();
     }
 
     private String getInput() {
         return this.receiver.obtainInput();
+    }
+
+    private void listFilesFromFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesFromFolder(fileEntry);
+            } else {
+                availableLanguages.add(fileEntry.getName());
+            }
+        }
+    }
+
+    private void displayAvailableLanguageOptions() {
+        for (int i = 0; i < availableLanguages.size(); i++) {
+            String str = availableLanguages.get(i);
+            str = str.substring(0, str.lastIndexOf('.'));
+            System.out.println(str);
+        }
     }
 }
